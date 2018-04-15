197fde97c5d2580f44e0abcb636c151603d81eaa
Zhenye <492684811@qq.com>
2018-04-15 04:34:16 -0400

    Add POST /users route and token method, hidden bug in User schema fixed

--- server/server.js
+++ server/server.js
@@ -6,11 +6,10 @@ const _ = require('lodash');
 const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-const port = process.env.PORT;
-
+var port = process.env.PORT;
 var app = express();
 
 app.use(bodyParser.json());
@@ -22,14 +21,14 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-    res.status(400).send(e);
+    res.status(400).send(e.message);
   });
 });
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send(todos);
-  }).catch((e) => res.send(e));
+  }).catch((e) => res.send(e.message));
 
 });
 
@@ -88,8 +87,30 @@ app.patch('/todos/:id', (req, res) => {
       res.send({todo});
     }
   }).catch((e) => {
-    res.status(400).send();
+    res.status(400).send(e.message);
   });
+});
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email','password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e.message);
+  });
+});
+
+app.get('/users', (req, res) => {
+  User.find().then((users) => {
+    res.send(users);
+  }).catch((e) => {
+    res.status(400).send(e.message);
+  });
+
 });
 
 app.listen(port, () => {
