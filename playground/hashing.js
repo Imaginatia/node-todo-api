83e05de66eaaa78f068a16d2031d5e43000754b0
Zhenye <492684811@qq.com>
2018-04-16 00:31:25 -0400

    Hashing user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,11 +1,25 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 5
-};
+var password = '123abc';
 
-var token = jwt.sign(data, 'secret');
-console.log(token);
-var decoded = jwt.verify(token, 'secret');
-console.log(decoded);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+var hash = '$2a$10$4JBRZkfVN49nMciyb0CJw.Oem1nnvxV6QieukhHWfmjZWyrqf1eOi';
+
+bcrypt.compare(password, hash, (err, res) => {
+  console.log(res);
+});
+// var data = {
+//   id: 5
+// };
+//
+// var token = jwt.sign(data, 'secret');
+// console.log(token);
+// var decoded = jwt.verify(token, 'secret');
+// console.log(decoded);
